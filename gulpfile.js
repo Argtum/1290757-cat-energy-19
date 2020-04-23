19971a62c4f6aaa36ea080c603ebda3614bef7d7
Alex Silver <Silwar40k@gmail.com>
2020-04-23 20:55:47 +0300

    Добавил минификацию html в сборку

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var del = require("del");
 var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -74,6 +75,7 @@ gulp.task("html", function () {
     .pipe(posthtml([
       include()
     ]))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
 });
 gulp.task("js", function () {
